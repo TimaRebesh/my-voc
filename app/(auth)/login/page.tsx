368ecda8f5d290590d35c39c245dfda23e6ad389
TimaRebesh <amitetoya@gmail.com>
2024-06-15 17:22:03 +0200

    option 4

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -32,7 +32,7 @@ export default async function () {
         </div>
         <ProvidersButtons />
         <p className="text-sm text-gray-400 whitespace-nowrap">
-          Created By: <span className="p-semibold-14">Timothy Rebesh 3</span>
+          Created By: <span className="p-semibold-14">Timothy Rebesh 4</span>
         </p>
       </div>
     </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,21 @@ import './globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from '@/components/ui/toaster';
 import Provider from '@/components/provider/Provider';
+import Head from 'next/head';
+import type { Viewport } from 'next';
+
+export const viewport: Viewport = {
+  maximumScale: 1,
+  userScalable: false
+};
+
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'My voc',
   description: 'My vocabulary',
+  viewport: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
 };
 
 export default function GlobalLayout({
@@ -19,6 +28,9 @@ export default function GlobalLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      {/* <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+      </Head> */}
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Provider>
