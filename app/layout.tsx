d509b1aa2c361daf779c0b43863d0b11aee90b26
TimaRebesh <amitetoya@gmail.com>
2024-06-15 16:28:29 +0200

    next/head

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,14 +4,13 @@ import './globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from '@/components/ui/toaster';
 import Provider from '@/components/provider/Provider';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'My voc',
   description: 'My vocabulary',
-  viewport: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
-
 };
 
 export default function GlobalLayout({
@@ -21,6 +20,9 @@ export default function GlobalLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      </Head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Provider>
