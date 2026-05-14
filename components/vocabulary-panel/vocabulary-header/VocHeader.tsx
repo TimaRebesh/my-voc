f8a36f466b471056fd117cca3c501ba4781409b2
TimaRebesh <amitetoya@gmail.com>
2026-05-14 22:42:34 +0200

    coutn

--- components/vocabulary-panel/vocabulary-header/VocHeader.tsx
+++ components/vocabulary-panel/vocabulary-header/VocHeader.tsx
@@ -1,8 +1,10 @@
+import { MAX_NUMBER_DEFINING_NEW } from '@/constants';
 import { IVocabulary, Word } from '@/lib/database/models/vocabulary.model';
-import { VocabularySelector } from './VocabularySelector';
+import { getWordProgress } from '@/lib/utils';
 import { IUser } from '@/lib/database/models/user.model';
 import { ITopic } from '@/lib/database/models/topic.model';
 import { NewWordCreator } from '../vocabulary-view/NewWordCreator';
+import { VocabularySelector } from './VocabularySelector';
 
 export const VocHeader = ({
   user,
@@ -15,15 +17,27 @@ export const VocHeader = ({
 }) => {
   return (
     <div className="bg-header h-12 flex items-center sticky">
-      <Counter count={currentVoc.list.length} />
+      <Counter list={currentVoc.list} />
       <VocabularySelector user={user} topic={topic} currentVoc={currentVoc} />
       <NewWordCreator vocId={currentVoc._id} />
     </div>
   );
 };
 
-const Counter = ({ count }: { count: number }) => (
-  <div className="text-gray-300 text-[8px] mx-1 sm:ml-6 sm:text-[10px]">
-    {count}
-  </div>
-);
+const Counter = ({ list }: { list: Word[]; }) => {
+  const allWordsCount = list.length;
+  const learnedWordsCount = list.filter(
+    (word) => getWordProgress(word) >= MAX_NUMBER_DEFINING_NEW
+  ).length;
+
+  return (
+    <div className="flex flex-col items-center justify-center leading-none text-gray-300 mx-1 sm:ml-6 min-w-5">
+      <span className="text-[11px] sm:text-[13px] font-semibold">
+        {allWordsCount}
+      </span>
+      <span className="text-[8px] sm:text-[9px] opacity-70 pt-1">
+        {learnedWordsCount}
+      </span>
+    </div>
+  );
+};
